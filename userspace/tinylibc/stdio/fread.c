7fd0d33c29c6c68f918b2408df0701e221d27867
PavelAtr <pvsamsonov76@gmail.com>
2024-12-06 03:39:51 +0300

    Development.

--- userspace/tinylibc/stdio/fread.c
+++ userspace/tinylibc/stdio/fread.c
@@ -2,19 +2,24 @@
 #include <syscall.h>
 #include <unistd.h>
 
+size_t fstrread(void* ptr, size_t size, size_t nmemb, FILE* stream)
+{
+    size_t len = size * nmemb;
+    size_t ret = (stream->pos + len > stream->size)? stream->size - stream->pos: len;
+    size_t i;
+    for (i = 0; i < ret; i++)
+        ((char*) ptr)[stream->pos + i] = stream->strbuf[i];
+    return ret;
+}
+
 size_t fread(void* ptr, size_t size, size_t nmemb, FILE* stream)
 {
     size_t ret;
-    size_t len;
 
     if (stream->strbuf)
     {
-	len = size * nmemb;
-	ret = (stream->pos + len > stream->size)? stream->size - stream->pos: len;
-	size_t i;
-	for (i = 0; i < ret; i++)
-	    ((char*) ptr)[stream->pos + i] = stream->strbuf[i];
-	return ret;
+	ret = fstrread(ptr, size,  nmemb, stream);
+	goto end;
     }
 
     ret = syscall(SYS_FREAD, stream->file, ptr, size * nmemb, stream->pos);
@@ -24,6 +29,7 @@ size_t fread(void* ptr, size_t size, size_t nmemb, FILE* stream)
 	stream->flags |= FILE_EOF;
 	stream->size = stream->pos;
     }
+end:
 //    if (!ret) usleep(1);
     return ret;
 }
--- userspace/tinylibc/stdio/fwrite.c
+++ userspace/tinylibc/stdio/fwrite.c
@@ -2,20 +2,24 @@
 #include <syscall.h>
 #include <unistd.h>
 
+size_t fstrwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream)
+{
+    size_t len = size * nmemb;
+    size_t ret = (stream->pos + len > stream->size)? stream->size - stream->pos: len;
+    size_t i;
+    for (i = 0; i < ret; i++)
+         stream->strbuf[i] = ((char*) ptr)[stream->pos + i];
+    return ret;
+}
 
 size_t fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream)
 {
     size_t ret;
-    size_t len;
 
     if (stream->strbuf)
     {
-	len = size * nmemb;
-	ret = (stream->pos + len > stream->size)? stream->size - stream->pos: len;
-	size_t i;
-	for (i = 0; i < ret; i++)
-	    stream->strbuf[i] = ((char*) ptr)[stream->pos + i];
-	return ret;
+	ret = fstrwrite(ptr, size, nmemb, stream);
+	goto end;
     }
     ret = syscall(SYS_FWRITE, stream->file, ptr, size * nmemb, stream->pos);
     stream->pos += ret;
@@ -24,6 +28,7 @@ size_t fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream)
 	stream->flags |= FILE_EOF;
 	stream->size =  stream->pos;
     }
+end:
 //    if (!ret) usleep(1);
     return ret;
 }
