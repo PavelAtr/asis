57fdf09aaf4726da2dad18eb00dec57163ebdf81
PavelAtr <pvsamsonov76@gmail.com>
2024-12-12 15:43:33 +0300

    Development.

---
+++ userspace/freebsd-src-stub.h
@@ -0,0 +1,6 @@
+#ifndef _FREEBSD_SRC_STUB_H
+#define _FREEBSD_SRC_STUB_H
+
+#define MB_CUR_MAX 2
+
+#endif
--- userspace/freebsd-src.sh
+++ userspace/freebsd-src.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-export CC=clang
-export HOST_CC=clang
+cd freebsd-src
+CWD=$(pwd)
+
+export CC=gcc
+export HOST_CC=gcc
 export PREFIX="/tinysys"
-export CFLAGS="-g -fPIC -fomit-frame-pointer -Wno-format -D__OFF_T_MATCHES_OFF64_T -I${PREFIX}/usr/include"
+export CFLAGS="-g -fPIC -fomit-frame-pointer -Wno-format -D__OFF_T_MATCHES_OFF64_T -include ${CWD}/../freebsd-src-stub.h -I${PREFIX}/usr/include"
 export LDFLAGS="-nostdlib ${PREFIX}/lib/crt.o ${PREFIX}/lib/libc.so"
 export TARGET="amd64"
 export MK_DIRDEPS_BUILD="yes"
 
-cd freebsd-src
-
-CWD=$(pwd)
 
 bmake -m ${CWD}/share/mk -B -C bin/sh
 
