b8c8c70aae5f1c36e06540202340ee306022e4a5
PavelAtr <pvsamsonov76@gmail.com>
2024-12-13 10:24:29 +0300

    Development.

--- userspace/freebsd-src-stub.h
+++ userspace/freebsd-src-stub.h
@@ -8,6 +8,16 @@ void bzero(void* s, size_t n);
 
 typedef unsigned int u_int;
 typedef unsigned char u_char;
+typedef unsigned char u_int8_t;
+typedef char *          caddr_t;        /* core address */
+typedef const char *    c_caddr_t;      /* core address, pointer to const */
 
+struct spacectl_range {
+        off_t   r_offset;       /* starting offset */
+        off_t   r_len;  /* length */
+};
+
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
 
 #endif
