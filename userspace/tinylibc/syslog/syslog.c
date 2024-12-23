e062870aaa6c55953a5bf4b89f1dcfbd5d45ce56
PavelAtr <pvsamsonov76@gmail.com>
2024-12-23 11:37:29 +0300

    Development.

---
+++ userspace/tinylibc/syslog/syslog.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+
+/* NOT REALIZED */
+
+void openlog(const char *ident, int option, int facility)
+{
+}
+
+void syslog(int priority, const char *format, ...)
+{
+}
+
+void closelog(void)
+{
+}
+
+void vsyslog(int priority, const char *format, va_list ap)
+{
+}
