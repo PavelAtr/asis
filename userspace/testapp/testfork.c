a439c2e1b1b465e7e9c99b70fd38bac7e4310a5a
PavelAtr <pvsamsonov76@gmail.com>
2025-02-06 13:17:14 +0300

    Development

--- userspace/testapp/testfork.c
+++ userspace/testapp/testfork.c
@@ -3,13 +3,16 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+__thread int a;
+
 int main(int argc, char** argv)
 {
    pid_t child1 = fork();
    if (child1 == 0) {
       int i;
+      a = 1;
       for (i = 0; i < 10; i++) {
-         puts("Child1\n");
+         printf("Child1 %d\n", a);
          usleep(1);
       }
       return 0;
@@ -17,8 +20,9 @@ int main(int argc, char** argv)
       pid_t child2 = fork();
       if (child2 == 0) {
          int i;
+         a = 2;
          for (i = 0; i < 10; i++) {
-            puts("Child2\n");
+            printf("Child2 %d\n", a);
             usleep(1);
          }
          return 0;
