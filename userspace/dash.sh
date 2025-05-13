f4172b274a91e3519dd0f56b38f5db727b892f66
PavelAtr <pvsamsonov76@gmail.com>
2025-05-14 01:30:05 +0300

    Development

--- userspace/dash.sh
+++ userspace/dash.sh
@@ -7,17 +7,14 @@ make clean
 ./configure
 make
 
-#export CC=gcc
 export PREFIX="/tinysys"
 export CFLAGS="-g -fPIC -fomit-frame-pointer --sysroot=/tinysys -D_Nullable= "
-export LDFLAGS="-Wl,--no-undefined --sysroot=/tinysys -nostdlib ${PREFIX}/lib/crt.o ${PREFIX}/lib/tls.a -ltinyc -shared"
-export EXTENTION=.so
+export LDFLAGS="-Wl,--no-undefined --sysroot=/tinysys -nostdlib ${PREFIX}/lib/crt.o ${PREFIX}/lib/tls.a -ltinyc"
 
 rm -rf src/*.o
 ./configure --host=x86_64-pc-none --build=x86_64-linux-gnu --target=x86_64-linux-gnu --disable-test-workaround --prefix=${PREFIX}
 make V=1
 make install
-mv ${PREFIX}/bin/dash ${PREFIX}/bin/dash${EXTENTION}
 
 
 
