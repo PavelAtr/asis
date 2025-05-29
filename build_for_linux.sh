74acf99e5b4f1c0ba95334535629866678ddafac
PavelAtr <pvsamsonov76@gmail.com>
2025-05-29 05:58:30 +0300

    Development

--- build_for_linux.sh
+++ build_for_linux.sh
@@ -3,17 +3,15 @@ set -e
 
 ./clean.sh
 
-export CC=gcc
 
 make -C userspace/alibc install_headers
-make -C core -f Makefile.linux install_headers
-make -C userspace/libpwd install_headers
 make -C userspace/libdl install_headers
-
-make -C userspace/alibc
+make -C userspace/libpwd install_headers
+make -C core -f Makefile.linux install_headers
+make -C userspace/alibc 
 make -C userspace/alibc install
-make -C userspace/libpwd
-make -C userspace/libpwd install
 make -C userspace/testapp
 make -C userspace/testapp install
+make -C userspace/libpwd
+make -C userspace/libpwd install
 make -C core -f Makefile.linux
